458bd6d457dbcd8525d06e3672e32938b219ae1b
Duwab <contact@superpaas.pro>
2019-03-05 16:10:22 +0100

    Mock different paths

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/path1', (req, res) => res.send({
+  route: "/path1"
+}));
+app.get('/path2', (req, res) => res.send({
+  route: "/path2"
+}));
 app.get('*', (req, res) => res.send({
-  data: "value"
+  default: req.url
 }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
